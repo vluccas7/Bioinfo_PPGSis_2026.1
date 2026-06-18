0700824644a8269d415712c9b7e1ecc8d7768ef4
Victor Rodrigues <155919771+vluccas7@users.noreply.github.com>
2026-06-18 12:47:24 +0000

    Atualizando

--- 1_dados/1.brutos/aula_03/Trimmomatic.fun.sh
+++ 1_dados/1.brutos/aula_03/Trimmomatic.fun.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 # Variaveis
+
 SP="Arapaima_gigas"
-R1="${SP}_R1.fastq.gz"
-R2="${SP}_R2.fastq.gz"
-R1_paired="../../2.limpos/${SP}_R1_paired.fastq.gz"
-R1_unpaired="../../2.limpos/${SP}_R1_unpaired.fastq.gz"
-R2_paired="../../2.limpos/${SP}_R2_paired.fastq.gz"
-R2_unpaired="../../2.limpos/${SP}_R2_unpaired.fastq.gz"
+R1="seqs/${SP}_R1.fastq.gz"
+R2="seqs/${SP}_R2.fastq.gz"
+R1_paired="limpos/${SP}_R1_paired.fastq.gz"
+R1_unpaired="limpos/${SP}_R1_unpaired.fastq.gz"
+R2_paired="limpos/${SP}_R2_paired.fastq.gz"
+R2_unpaired="limpos/${SP}_R2_unpaired.fastq.gz"
+
+# Rodando
+
+trimmomatic PE \
+    "$R1" "$R2" \
+    "$R1_paired" "$R1_unpaired" \
+    "$R2_paired" "$R2_unpaired" \
+    ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+    LEADING:3 \
+    TRAILING:3 \
+    SLIDINGWINDOW:4:20\
+    MINLEN:50
+
+echo -n "GATTACCTGATTGCCCGTCCGATAAGCTCACTATCAATACAGAACGTCAATAGAATGGCCATGCTGTACAAGATTGTACCTAGTAACTGCTCTTTAGAGCAGATAGTATCCTGAGATCGGAAGAGCACACGTCTGAACTCCAGTCA" | wc -c
+# 146
+
+echo -n "EDEEFEGGGIIGFGEFHDGDDDEFIDDFHFHHIGDDHDDGHHGGIGHEEDEHHFGDFGEIGFEIGDEIDEDEHFEIGFEGFIHIGHDGHFIEFFHEFEDIHIEHFFFFDDIGFFIFFIGFIEEDDEEEIGAEECED;A@?;;A;@@?:;:" | wc -c
+# 150
