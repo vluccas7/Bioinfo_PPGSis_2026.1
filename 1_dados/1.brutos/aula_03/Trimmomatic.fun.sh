db12ce23847a8b6e70c3aa319801b78b3af779a9
Victor Rodrigues <155919771+vluccas7@users.noreply.github.com>
2026-06-23 01:41:14 +0000

    resolve conflicts

--- 1_dados/1.brutos/aula_03/Trimmomatic.fun.sh
+++ 1_dados/1.brutos/aula_03/Trimmomatic.fun.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Variaveis
 
-SP="Arapaima_gigas"
-R1="seqs/${SP}_R1.fastq.gz"
-R2="seqs/${SP}_R2.fastq.gz"
+SP="SRR13510367"
+R1="../aula_04/${SP}_1.fastq.gz"
+R2="../aula_04/${SP}_2.fastq.gz"
 R1_paired="limpos/${SP}_R1_paired.fastq.gz"
 R1_unpaired="limpos/${SP}_R1_unpaired.fastq.gz"
 R2_paired="limpos/${SP}_R2_paired.fastq.gz"
